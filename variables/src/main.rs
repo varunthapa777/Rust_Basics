718f4d8317ae38969543255214bd4645c41370d7
varunthapa777 <varunthapa1920@gmail.com>
2024-09-27 19:38:39 +0530

    Refactor variable handling and introduce shadowing
    This commit refactors the variable handling in the main.rs file. It introduces the concept of immutability by default and demonstrates how to make variables mutable using the `mut` keyword. Additionally, it showcases the use of shadowing to create new variables with the same name but different values or types. The code also includes examples of using variables inside and outside of blocks. Overall, this commit improves the clarity and readability of the code related to variables.

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,9 +1,38 @@
 fn main() {
-    let mut a = 5;
 
+    // variable are immutable by default
+    // let a = 5;
+
+    // a = 6; // error: cannot assign twice to immutable variable `a`
+
+    
+    // Use mut keyword to make variable mutable
+    let mut a = 5;
+    
     println!("a = {a}");
     
-    a = 10;
+    a = 6;
     
     println!("a = {a}");
+
+    // shadowing
+    let s = 20;
+
+    let s = s + 1;
+
+
+    {
+        let s = s * 2;
+
+        println!("Value of variable inside block: {s}");
+    }
+
+    println!("Value of variable outside block: {s}");
+    
+    // shadowing with different type
+    let spaces = "   ";
+    let spaces = spaces.len();
+
+    println!("Value of spaces: {spaces}");
+   
 }
