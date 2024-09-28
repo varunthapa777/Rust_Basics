f2a9f3f26c296fd01c00cd1d0d13640af3e95fd7
varunthapa777 <varunthapa1920@gmail.com>
2024-09-28 08:21:36 +0530

    Learn Rust datatypes

---
+++ datatypes/src/main.rs
@@ -0,0 +1,86 @@
+fn main() {
+    println!("Data Types in Rust");
+
+    /* 
+    // Scalar Types
+
+    // 1. Integer Types
+
+    let a: u8 = 255; // 8-bit unsigned integer
+    let b: i8 = -128; // 8-bit signed integer
+    let c: u16 = 65535; // 16-bit unsigned integer
+    let d: i16 = -32768; // 16-bit signed integer
+    let e: u32 = 4294967295; // 32-bit unsigned integer
+    let f: i32 = -2147483648; // 32-bit signed integer
+    let g: u64 = 18446744073709551615; // 64-bit unsigned integer
+    let h: i64 = -9223372036854775808; // 64-bit signed integer
+    let i: u128 = 340282366920938463463374607431768211455; // 128-bit unsigned integer
+    let j: i128 = -170141183460469231731687303715884105728; // 128-bit signed integer
+    let k: isize = 9223372036854775807; // signed integer, depends on the architecture
+    let l: usize = 18446744073709551615; // unsigned integer, depends on the architecture
+
+    // 2. Floating-Point Types
+
+    let a: f32 = 3.14; // 32-bit floating-point number or single precision
+    let b: f64 = 3.141592653589793; // 64-bit floating-point number or double precision
+    
+    // operations
+
+    let sum = 5 + 10;
+    let difference = 95.5 - 4.3;
+    let product = 4 * 30;
+    let quotient = 56.7 / 32.2;
+    let truncated_quotient = 43 / 5;
+    let remainder = 43 % 5;
+
+    println!("trucated_quotient: {}", truncated_quotient);
+
+    // 3. Boolean Type
+    let a: bool = true;
+    let b: bool = false;
+
+    // 4. Character Type
+
+    let a: char = 'A';
+
+    */
+    
+
+    /* 
+    // Compound Types
+
+    // 1. Tuple Type
+
+    let tup: (i32, f64, u8) = (500, 6.4, 1);
+
+    // destructuring a tuple
+    let (x, y, z) = tup;
+
+    println!("x y z: {} {} {}", x, y, z);
+
+
+    // access elements of a tuple using dot notation
+    println!("tup.0 tup.1 tup.2: {} {} {}", tup.0, tup.1, tup.2);
+
+
+    // 2. Array Type
+
+    // decalaration
+    let a1: [i32; 5];
+
+
+    // initialization
+    let a2 = [1, 2, 3, 4, 5];
+
+
+    // initialization with a default value
+    let a3 = [3; 5]; // [3, 3, 3, 3, 3]
+
+
+    // access elements of an array
+    let b = a3[3]; // 4
+    println!("b: {}", b);
+
+    */
+
+}
