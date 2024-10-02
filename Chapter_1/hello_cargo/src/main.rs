d8e0a0f5782e116973ca25d6a2818b9b4b867cb8
varunthapa777 <varunthapa1920@gmail.com>
2024-10-02 17:37:16 +0530

    Add print statements and create String variable in main.rs

--- Chapter_1/hello_cargo/src/main.rs
+++ Chapter_1/hello_cargo/src/main.rs
@@ -1,3 +1,6 @@
-fn main() {
+fn main(){
     println!("Hello, world!");
+    println!("Hello");
+    let s: String = String::from("Hii");
 }
+
---
+++ Chapter_6/enums/src/main.rs
@@ -0,0 +1,16 @@
+#[derive(Debug)]
+enum IpAddr {
+    V4(String),
+    V6(String),
+}
+
+fn main() {
+    let home = IpAddr::V4(String::from("127.0.0.1"));
+    let loopback = IpAddr::V6(String::from("::1"));
+    route(home);
+    route(loopback);
+}
+
+fn route(ip: IpAddr) {
+    println!("{:?}", ip);
+}
